f896af9cc7892552a9714d6d2fc32acef025c45b
Amit Kumar Singh <91126008+XxFiEnDxX@users.noreply.github.com>
2024-10-28 22:29:26 +0530

    catalan number

---
+++ DP (1D)/catalan_number.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int catalan(int  n){
+    if(n <= 1)return 1;
+
+    int res = 0;
+    for(int i = 0; i < n; i++){
+        res += catalan(i)*catalan(n-i-1);
+    }
+
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i = 0; i <= n; i++){
+        cout<<catalan(i)<<" ";
+    }
+    return 0;
+}
